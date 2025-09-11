23df52bb49db1faeff4245d8f106bf31eb6bb274
IamMark19 <markanthonyudarbe19@gmail.com>
2025-09-11 17:21:26 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/coffeeshop-react-springboot_v2/', // 👈 important for GitHub Pages
+  base: '/', // 👈 important for GitHub Pages
 })
