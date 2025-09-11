f8da2d23cf20aa05a2d732d6bc399d037cd0faeb
maudarbe <70473807+IamMark19@users.noreply.github.com>
2025-09-11 14:13:09 +0800

    backend initil commit springboot
    backend initil commit springboot

---
+++ backend/src/test/java/com/coffeeshop/cms/CoffeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
